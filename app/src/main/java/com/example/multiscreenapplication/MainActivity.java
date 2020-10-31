28f5ab5993144bfe037c54d6f4050b0449c3744e
Saish Shenoy <saish112@gmail.com>
2020-10-31 19:33:07 +0530

    Keep on moving across 2 pages back and forth

--- app/src/main/java/com/example/multiscreenapplication/MainActivity.java
+++ app/src/main/java/com/example/multiscreenapplication/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.multiscreenapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextClock;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static int n = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView1 = (TextView) findViewById(R.id.idiot1);
+        textView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(n == 0)Toast.makeText(v.getContext(), "You have been entertained one more time!!", Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(MainActivity.this, IdiotActivity.class);
+                startActivity(i);
+            }
+        });
     }
+
 }
